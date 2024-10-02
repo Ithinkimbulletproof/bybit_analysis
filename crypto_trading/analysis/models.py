7747a408f1d2c330304e62248ea8578282d245f6
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-10-02 17:12:56 +0500

    start project

--- crypto_trading/analysis/models.py
+++ crypto_trading/analysis/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 
 class Coin(models.Model):
-    symbol = models.CharField(
-        max_length=10)  # символ монеты (например, BTCUSDT)
-    settings = models.JSONField(null=True,
-                                blank=True)  # настройки для автотрейдинга
-    analysis_result = models.TextField(null=True,
-                                       blank=True)  # результат анализа
+    symbol = models.CharField(max_length=10)  # символ монеты (например, BTCUSDT)
+    settings = models.JSONField(null=True, blank=True)  # настройки для автотрейдинга
+    analysis_result = models.TextField(null=True, blank=True)  # результат анализа
 
     def __str__(self):
         return self.symbol
