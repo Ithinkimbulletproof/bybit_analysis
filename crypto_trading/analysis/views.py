2ef3d22d836bc22bab78a7e9abb8acbc398bf0ac
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-10-03 12:42:48 +0500

    add project name

--- crypto_trading/analysis/views.py
+++ crypto_trading/analysis/views.py
@@ -3,14 +3,11 @@ from .models import Coin
 from .utils import perform_analysis
 
 def analyze_view(request, symbol):
-    # Получаем настройки и результаты анализа через utils.analyze_coin
-    settings, analysis = perform_analysis(symbol)  # Используем правильное название функции
+    settings, analysis = perform_analysis(symbol)
 
-    # Сохраняем или обновляем информацию о монете в базе данных
     coin, created = Coin.objects.get_or_create(symbol=symbol)
-    coin.settings = settings  # Настройки для автотрейдинга
-    coin.analysis_result = analysis.to_html()  # Преобразуем DataFrame в HTML для отображения в шаблоне
+    coin.settings = settings
+    coin.analysis_result = analysis.to_html()
     coin.save()
 
-    # Отправляем результат в шаблон для отображения
-    return render(request, "analysis/result.html", {"coin": coin})
+    return render(request, "analysis/result.html", {"coin": coin, "project_name": "Fazanka Records Crypto Trading"})
